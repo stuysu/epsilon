2440ace752c1f194751f5b1133e482899b7e1f8f
Randy <randysim9@gmail.com>
2024-02-08 19:45:21 -0500

    update theme

--- src/comps/context/ThemeProvider.tsx
+++ src/comps/context/ThemeProvider.tsx
@@ -37,28 +37,10 @@ const ThemeProvider = ({ children } : { children: React.ReactNode }) => {
 						main: "#546de5",
 						contrastText: "#fff"
 					},
-					button: {
-						main: prefersDarkMode ? "rgba(255, 255, 255, 0.87)" : "rgba(0, 0, 0, 0.87)"
-					},
 					background: {
 						default: prefersDarkMode ? "#262630" : "#FFFFFF",
 						paper: prefersDarkMode ? "#262630" : "#FFFFFF"
-					},
-					transparency: prefersDarkMode
-						? {
-								background: "rgba(255, 255, 255, 0.08)",
-								border: "rgba(255, 255, 255, 0.1)",
-								borderDarker: "rgba(255, 255, 255, 0.24)",
-								text: "rgba(255, 255, 255, 0.8)",
-								textLighter: "rgba(255, 255, 255, 0.4)"
-						  }
-						: {
-								background: "rgba(0, 0, 0, 0.08)",
-								border: "rgba(0, 0, 0, 0.1)",
-								borderDarker: "rgba(0, 0, 0, 0.24)",
-								text: "rgba(0, 0, 0, 0.8)",
-								textLighter: "rgba(0,0,0,0.4)"
-						  }
+					}
 				},
 				typography: {
 					fontFamily: `'Roboto Condensed', sans-serif`,
@@ -94,31 +76,6 @@ const ThemeProvider = ({ children } : { children: React.ReactNode }) => {
 						margin: "0.5rem 0"
 					}
 				},
-				components: {
-					MuiPaper: {
-						styleOverrides: {
-							root: {
-								backgroundImage: "unset",
-								boxShadow: prefersDarkMode
-									? "0px 2px 1px -1px rgba(255,255,255,0.2),0px 1px 1px 0px rgba(255,255,255,0.14),0px 1px 3px 0px rgba(255,255,255,0.12)"
-									: "" // change from {} to match types
-							}
-						}
-					},
-					MuiTab: {
-						styleOverrides: { root: { minWidth: "160px" } }
-					}
-				},
-				// restores legacy breakpoint values
-				breakpoints: {
-					values: {
-						xs: 0,
-						sm: 600,
-						md: 960,
-						lg: 1280,
-						xl: 1920
-					}
-				}
 			}),
 		[prefersDarkMode]
 	);
