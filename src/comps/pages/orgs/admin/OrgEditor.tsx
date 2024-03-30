b386c8d2f020a82800b96a344b4793957764c439
Randy <randysim9@gmail.com>
2024-03-30 18:18:21 -0400

    client side validation indication

--- src/comps/pages/orgs/admin/OrgEditor.tsx
+++ src/comps/pages/orgs/admin/OrgEditor.tsx
@@ -1,6 +1,44 @@
-import { useState } from "react";
-import { Box, TextField } from "@mui/material";
+import { useState, ChangeEvent } from "react";
+import { Box, Button, TextField } from "@mui/material";
 
+const findDiff = (obj1 : any, obj2 : any) => {
+    let diff : ({ key: string, value1: any, value2: any })[] = []
+
+    for (let key of Object.keys(obj1)) {
+        if (
+            !(key in obj2) ||
+            obj1[key] === undefined ||
+            obj2[key] === undefined ||
+            (!obj1[key] && !obj2[key]) ||
+            obj1[key] == obj2[key]
+        ) continue;
+
+        diff.push({ key, value1: obj1[key], value2: obj2[key] })
+    }
+
+    return diff;
+}
+/* apply obj1 on obj2. */
+const apply = (obj1 : any, obj2: any) => {
+    let obj3 : any = {}
+
+    for (let key of Object.keys(obj1)) {
+        if (obj1[key]) {
+            obj3[key] = obj1[key]
+        } else {
+            obj3[key] = obj2[key]
+        }
+    }
+
+    return obj3;
+}
+
+/* 
+TextField Statuses:
+- default is Approved
+- once changed but not saved is Unsaved
+- once changed and saved is Pending
+*/
 const OrgEditor = (
     { organization, organizationEdit } : 
     { 
@@ -8,30 +46,69 @@ const OrgEditor = (
         organizationEdit: OrganizationEdit
     }
 ) => {
-    let orgEdit = useState(organizationEdit)
+    let [orgEdit, setOrgEdit] = useState(organizationEdit)
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        
+        setOrgEdit({
+            ...orgEdit,
+            [name]: value
+        })
+    }
+
+    /* 
+    STEPS 
+    - compare all fields that are different between organization and intersection of organizationEdit and organization
+    - upsert an organization edit with all different values
+    */
+    let createEdit = () => {
+        let diff = findDiff(apply(organizationEdit, organization), orgEdit);
+    }
 
     return (
         <div>
-            <h1>Organization Data</h1>
+            <h1>Organization</h1>
             <Box>
                 {
                     Object.keys(organizationEdit).map((field, i) => {
                         type orgKey = keyof OrganizationEdit & keyof Organization;
                         let key : orgKey = (field as orgKey);
 
-                        // @ts-ignore - this is definitely a key in orgEdit
-                        let current = orgEdit[key] || organization[key] || ""
+                        let current : any = orgEdit[key]
+                        if (current === undefined) {
+                            current = organization[key]
+                        }
+
+                        let status = "Approved"
+                        if (orgEdit[key] !== undefined) {
+                            if (
+                                organizationEdit[key] !== orgEdit[key] &&
+                                orgEdit[key] != (organization[key] || "")
+                            ) {
+                                status = "Unsaved"
+                            }
+                        } else if (organizationEdit[key] !== undefined) {
+                            status = "Pending"
+                        }
 
                         return (
                             <Box key={i} bgcolor="background.default" color="primary.contrastText">
                                 <TextField
                                 variant="filled"
+                                name={field}
                                 label={field}
-                                value={current}
+                                value={current || ""}
+                                onChange={onChange}
                                 />
+                                {status}
                             </Box>
                         )
                     })
+                }
+
+                {
+                    findDiff(apply(organizationEdit, organization), orgEdit).length !== 0 && <Button>Save</Button>
                 }
             </Box>
 
