8624fbce54c419efa3752105167f83d5cab4a833
Randy <randysim9@gmail.com>
2024-04-20 13:52:19 -0400

    create org function readded

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -8,18 +8,36 @@ import FormTextField from "../comps/ui/forms/FormTextField";
 import FormDropSelect from "../comps/ui/forms/FormDropSelect";
 import FormCheckSelect from "../comps/ui/forms/FormCheckSelect";
 
-const emptyForm = {
+import { supabase } from "../supabaseClient";
+
+type FormType = {
+  name: string,
+  url: string,
+  picture?: File,
+  mission: string,
+  purpose: string,
+  benefit: string,
+  appointment_procedures: string,
+  uniqueness: string,
+  meeting_schedule: string,
+  meeting_days: string,
+  keywords: string,
+  commitment_level: string,
+  join_instructions: string
+}
+
+const emptyForm : FormType = {
   name: "",
   url: "",
-  picture: null,
+  picture: undefined,
   mission: "",
   purpose: "",
   benefit: "",
   appointment_procedures: "",
   uniqueness: "",
   meeting_schedule: "",
-  meeting_days: [],
-  keywords: [],
+  meeting_days: "",
+  keywords: "",
   commitment_level: "",
   join_instructions: ""
 }
@@ -28,18 +46,99 @@ const Create = () => {
   const user = useContext(UserContext);
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState(emptyForm);
+  const [formData, setFormData] = useState<FormType>(emptyForm);
 
-  const onSubmit = async () => {
+  const createActivity = async () => {
+    let payload = {
+      creator_id: user.id,
+      name: formData.name,
+      url: formData.url,
+      picture: null, // update after creating initial org
+      mission: formData.mission,
+      purpose: formData.purpose,
+      benefit: formData.benefit,
+      appointment_procedures: formData.appointment_procedures,
+      uniqueness: formData.uniqueness,
+      meeting_schedule: formData.meeting_schedule,
+      meeting_days: formData.meeting_days,
+      keywords: formData.keywords,
+      commitment_level: formData.commitment_level,
+      join_instructions: formData.join_instructions,
+    }
 
-  }
+    let { data: orgCreateData, error: orgCreateError } = await supabase
+      .from("organizations")
+      .insert(payload)
+      .select(`
+        id
+      `);
+
+    if (orgCreateError || !orgCreateData) {
+      return user.setMessage(
+        "Error creating organization. Contact it@stuysu.org for support.",
+      );
+    }
+
+    let orgId = orgCreateData[0].id;
+
+    /* Create picture if organization is successfully created */
+    
+    /* convert picture to url */
+    if (formData.picture) {
+      let filePath = `org-pictures/${orgId}/${Date.now()}-${formData.picture.name}`
+      let { data: storageData, error: storageError } = await supabase
+        .storage
+        .from('public-files')
+        .upload(filePath, formData.picture);
+      
+      if (storageError || !storageData) {
+        return user.setMessage(
+          "Error uploading image to storage. Contact it@stuysu.org for support."
+        )
+      }
+
+      let { data: urlData } = await supabase
+      .storage
+      .from('public-files')
+      .getPublicUrl(filePath)
+
+      if (!urlData) {
+        /* attempt to delete organization */
+        await supabase
+          .from('organizations')
+          .delete()
+          .eq('id', orgId)
+
+        return user.setMessage("Failed to retrieve image after upload. Contact it@stuysu.org for support.");
+      }
+
+      
+      let { error: updateUrlError } = await supabase
+          .from("organizations")
+          .update({ picture: urlData.publicUrl })
+          .eq('id', orgId)
+
+      if (updateUrlError) {
+        /* attempt to delete organization */
+        await supabase
+          .from('organizations')
+          .delete()
+          .eq('id', orgId)
+        return user.setMessage("Error uploading image to organization. Contact it@stuysu.org for support.")
+      }
+    }
+    
+    user.setMessage("Organization created!");
+    /* redirect after creation */
+    navigate(`/${formData.url}`)
+  };
   
   return (
     <MultiPageForm
       title="Create New Organization" 
       value={formData} 
       onFormChange={setFormData}
-      onSubmit={onSubmit}
+      onSubmit={createActivity}
       submitText="Create Activity"
       width="100%"
       height="600px"
