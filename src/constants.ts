d53d656b05c6e437c47480237b90174a9adc1d7d
Randy <randysim9@gmail.com>
2024-04-27 17:33:58 -0400

    change env variable names to react format

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const PUBLIC_URL = process.env.PUBLIC_URL;
+export const PUBLIC_URL = process.env.REACT_APP_PUBLIC_URL;
 export const SUPABASE_URL =
-  process.env.SUPABASE_URL || "http://localhost:8000/";
+  process.env.REACT_APP_SUPABASE_URL || "http://localhost:8000/";
 export const SUPABASE_ANON_KEY =
-  process.env.SUPABASE_ANON_KEY ||
+  process.env.REACT_APP_SUPABASE_ANON_KEY ||
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzEzNDk5MjAwLAogICJleHAiOiAxODcxMjY1NjAwCn0.2oxGEWstIVA-SvFiq3Ebavd5LDEXIkmqtjTnDBQe0tw";
-export const HOSTNAME = process.env.HOSTNAME || "localhost:3000"
+export const HOSTNAME = process.env.REACT_APP_HOSTNAME || "localhost:3000"
