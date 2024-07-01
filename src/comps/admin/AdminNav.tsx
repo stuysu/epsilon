a541b6ee4917d057f7f666743d8e882a6adbef5c
Randy <randysim9@gmail.com>
2024-06-30 23:26:49 -0400

    feat(adminNav): added route tabs

--- src/comps/admin/AdminNav.tsx
+++ src/comps/admin/AdminNav.tsx
@@ -1,19 +1,35 @@
-import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import RouteTabs from "../ui/RouteTabs";
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
+import EditIcon from '@mui/icons-material/Edit';
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+import EmailIcon from "@mui/icons-material/Email";
 
 const AdminNav = () => {
+    let navLinks = [
+        {
+            to: "/admin/approve-pending",
+            label: "Pending Orgs",
+            icon: <PendingActionsIcon />
+        },
+        {
+            to: "/admin/approve-edit",
+            label: "Approve Edits",
+            icon: <EditIcon />
+        },
+        {
+            to: "/admin/strikes",
+            label: "Strikes",
+            icon: <ReportProblemIcon />
+        },
+        {
+            to: "/admin/send-message",
+            label: "Send Message",
+            icon: <EmailIcon />
+        },
+    ];
+
     return (
-        <Box>
-            <Link to="/admin/approve-pending">Pending Orgs</Link>
-            <br />
-            <Link to="/admin/approve-edit">Approve Edits</Link>
-            <br />
-            <Link to="/admin/strikes">Strikes</Link>
-            <br />
-            <Link to="/admin/send-email">Send Email</Link>
-            <br />
-            <Link to="/admin/send-message">Send Message</Link>
-        </Box>
+        <RouteTabs tabs={navLinks} />
     );
 };
 
