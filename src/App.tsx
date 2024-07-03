85e9a12b23da5eb2a6c057ae8b9d33abea08093a
SnowyNate <nmoy70@stuy.edu>
2024-07-03 19:55:40 -0400

    Implemented a custom scrollbar to replace the classic scrollbar with an overlay scrollbar that does not affect the position of components on the page. The scrollbar is fully customizable, and has been set to autohide as well as autosize based on the page's content. There is also a listener so that the scrollbar is rerendered upon window resizing to prevent a bug.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { BrowserRouter } from "react-router-dom";
@@ -10,9 +11,34 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { CssBaseline } from "@mui/material";
 
 import { SnackbarProvider } from "notistack";
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 const App = () => {
+    const [autoHeightMax, setAutoHeightMax] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setAutoHeightMax(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function to remove the event listener
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
+        <Scrollbars 
+        universal
+        autoHeight
+        autoHeightMin={100}
+        autoHeightMax={autoHeightMax}
+         autoHide
+        // Hide delay in ms
+        autoHideTimeout={1000}
+        // Duration for hide animation in ms.
+        autoHideDuration={200}
+        >
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <ThemeProvider>
                 <CssBaseline />
@@ -25,6 +51,7 @@ const App = () => {
                 </SnackbarProvider>
             </ThemeProvider>
         </LocalizationProvider>
+        </Scrollbars>
     );
 };
 
