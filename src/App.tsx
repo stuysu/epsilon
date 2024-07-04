2e29b258d75901033217f92abcd05f55456cd2a3
SnowyNate <nmoy70@stuy.edu>
2024-07-03 20:58:28 -0400

    Changed color to grey so that the scrollbar is visible in both light mode and dark mode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, CSSProperties } from "react";
 import "./App.css";
-
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./comps/context/ThemeProvider";
 import UserProvider from "./comps/context/UserProvider";
 import Pages from "./pages";
-
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { CssBaseline } from "@mui/material";
-
 import { SnackbarProvider } from "notistack";
 import { Scrollbars } from 'react-custom-scrollbars-2';
 
@@ -27,30 +24,38 @@ const App = () => {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
+    // Custom thumb component with type annotations
+    const renderThumb = ({ style, ...props }: { style: CSSProperties, [key: string]: any }) => {
+        const thumbStyle: CSSProperties = {
+            backgroundColor: 'grey',
+            borderRadius: '4px', // Adjust this value as needed to match your design
+        };
+        return <div style={{ ...style, ...thumbStyle }} {...props} />;
+    };
+
     return (
         <Scrollbars 
-        universal
-        autoHeight
-        autoHeightMin={100}
-        autoHeightMax={autoHeightMax}
-         autoHide
-        // Hide delay in ms
-        autoHideTimeout={1000}
-        // Duration for hide animation in ms.
-        autoHideDuration={200}
+            universal
+            autoHeight
+            autoHeightMin={100}
+            autoHeightMax={autoHeightMax}
+            autoHide
+            autoHideTimeout={1000}
+            autoHideDuration={200}
+            renderThumbVertical={renderThumb} // Apply the custom thumb component here
         >
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <ThemeProvider>
-                <CssBaseline />
-                <SnackbarProvider maxSnack={4} autoHideDuration={3000}>
-                    <BrowserRouter>
-                        <UserProvider>
-                            <Pages />
-                        </UserProvider>
-                    </BrowserRouter>
-                </SnackbarProvider>
-            </ThemeProvider>
-        </LocalizationProvider>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <ThemeProvider>
+                    <CssBaseline />
+                    <SnackbarProvider maxSnack={4} autoHideDuration={3000}>
+                        <BrowserRouter>
+                            <UserProvider>
+                                <Pages />
+                            </UserProvider>
+                        </BrowserRouter>
+                    </SnackbarProvider>
+                </ThemeProvider>
+            </LocalizationProvider>
         </Scrollbars>
     );
 };
