a1b68908c0f320ff5b5e5dc7a83eca0d5b4dff2d
David Chen <dchen07@protonmail.com>
2024-07-30 21:45:29 -0400

    Reword not found page
    Adds randomized flavor text to provide value for users, removes
    explicit reference to an HTTP error code (which we do not use).

--- src/pages/orgs/NotFound.tsx
+++ src/pages/orgs/NotFound.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Typography, Button } from "@mui/material";
 
+const FlavorText = [
+    "The page is a lie",
+    "I'm sorry Dave, I'm afraid I can't do that.",
+    ":(",
+    "PC LOAD LETTER",
+    "Keyboard not found. Press any key to continue...",
+    "Abort, Retry, Fail?",
+    "Success",
+    "Have you tried turning it off and on again?",
+    "Thank you! But our page is in another castle!",
+    "This is fine. 🔥",
+    "Imagine spamming refresh to see all the 404 easter eggs and stumbling upon this one first…",
+];
+
 const NotFound = () => {
     const navigate = useNavigate();
 
@@ -19,11 +33,10 @@ const NotFound = () => {
             p={3}
         >
             <Typography variant="h1" gutterBottom>
-                404 - Page Not Found
+                Page Not Found
             </Typography>
             <Typography variant="body1" gutterBottom>
-                Although Epsilon encompasses many services for Stuyvesant, it
-                unfortunately does not have everything.
+                {FlavorText[Math.floor(Math.random() * FlavorText.length)]}
             </Typography>
             <Button
                 variant="contained"
