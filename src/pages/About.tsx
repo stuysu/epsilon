499674cccae797be9a2b81bb5901011ada2ef5a0
David Chen <dchen07@protonmail.com>
2024-08-10 22:44:33 -0400

    Update credits

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -79,12 +79,17 @@ const About = () => {
                 <Credit title="The Original Epsilon Team">
                     Randy Sim, SU IT Co-Director '23-'24, SU IT '22-'23 <br />
                     <br />
-                    David Chen, SU IT Co-Director '23-'24, SU IT '21-'23 <br />
+                    David Chen, SU IT Co-Director '23-'25, SU IT '21-'23 <br />
                     <br />
-                    Nathaniel Moy, SU IT '23-'24
+                    Rahul Deb, SU IT Co-Director '24-'25, SU IT '23-'24 <br />
+                    <br />
+                    Nathaniel Moy, SU IT '23-'25 <br />
+                    <br />
+                    Adam Choi, SU IT '23-'25
+                    <br />
                 </Credit>
                 <Break />
-                <Credit title="SU IT Department Team 23-24:">
+                <Credit title="SU IT 23-24">
                     Randy Sim, SU IT Co-Director '23-'24, SU IT '22-'23 <br />
                     <br />
                     David Chen, SU IT Co-Director '23-'24, SU IT '21-'23 <br />
@@ -98,7 +103,7 @@ const About = () => {
                     Richard Wan, SU IT ’23-’24 <br />
                     <br />
                 </Credit>
-                <Credit title="SU IT Department Team 22-23:">
+                <Credit title="SU IT 22-23">
                     Yuhao “Ben” Pan, SU IT Co-Director ’22-’23, SU IT ’21-’22{" "}
                     <br />
                     <br />
