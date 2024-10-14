26627792ffab11e1be75f1cb77b3decd83fe96a5
Nathaniel <145233084+SnowyNate@users.noreply.github.com>
2024-10-14 15:58:47 -0400

    Revert constants.ts from local database to remote database

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const PUBLIC_URL =
     process.env.REACT_APP_PUBLIC_URL || "http://localhost:3000";
 export const SUPABASE_URL =
-    process.env.REACT_APP_SUPABASE_URL || "http://localhost:8000";
+    process.env.REACT_APP_SUPABASE_URL || "https://base.stuysu.org/";
 export const SUPABASE_ANON_KEY =
     process.env.REACT_APP_SUPABASE_ANON_KEY ||
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
