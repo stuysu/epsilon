b395e0bb56820654fa8986ff700bec9da42a43b2
Daniel Yang <danielwzyang@gmail.com>
2024-12-13 10:01:17 -0500

    reused rahul's code from 10/18 for approval all member requests and club admin emails

--- src/pages/orgs/admin/MemberRequests.tsx
+++ src/pages/orgs/admin/MemberRequests.tsx
@@ -1,11 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import OrgContext from "../../../comps/context/OrgContext";
 
 import PendingMember from "../../../comps/pages/orgs/admin/PendingMember";
 import { Box, Typography } from "@mui/material";
+import AsyncButton from "../../../comps/ui/AsyncButton";
 
 const MemberRequests = () => {
     const organization = useContext<OrgContextType>(OrgContext);
+    const [autoApprove, setAutoApprove] = useState(false);
     const pendingMembers = organization.memberships
         ?.filter((member) => !member.active)
         .map((member) => {
@@ -17,12 +19,25 @@ const MemberRequests = () => {
                 picture: member.users?.picture,
             };
         });
+    
+    const handleApproveAll = () => {
+        setAutoApprove(true);
+    };
 
     return (
         <Box sx={{ width: "100%" }}>
             <Typography variant="h1" align="center" width="100%">
                 Member Requests
             </Typography>
+            <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "1em", marginTop: "1em" }}>
+                <AsyncButton
+                    onClick={handleApproveAll}
+                    variant="contained"
+                    sx={{ height: "40px"}}
+                >
+                    Approve All
+                </AsyncButton>
+            </Box>
             {pendingMembers?.map((member, i) => (
                 <PendingMember
                     id={member.membershipId || -1}
@@ -31,6 +46,7 @@ const MemberRequests = () => {
                     email={member.email || "Undefined"}
                     picture={member.picture}
                     key={i}
+                    auto={autoApprove}
                 />
             ))}
         </Box>
