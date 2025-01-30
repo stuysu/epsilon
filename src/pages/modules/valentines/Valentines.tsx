516ea7ace4d42734853bc4f1485a22cd48894940
David Chen <dchen07@protonmail.com>
2025-01-29 20:59:54 -0500

    Add fetch all valentines query

--- src/pages/modules/valentines/Valentines.tsx
+++ src/pages/modules/valentines/Valentines.tsx
@@ -1,5 +1,47 @@
+import AsyncButton from "../../../comps/ui/AsyncButton";
+import { useState } from "react";
+import { supabase } from "../../../supabaseClient";
+import { enqueueSnackbar } from "notistack";
+
+interface Valentine {
+    id: number;
+    sender: number;
+    receiver: number;
+    show_sender: boolean;
+
+    message: string;
+    background: string;
+}
 const Valentines = () => {
-    return <p>valentine</p>;
+    const [valentines, setValentines] = useState([]);
+
+    return (
+        <>
+            <h1>Valentines Testing</h1>
+            <AsyncButton
+                onClick={async () => {
+                    const { data, error } = await supabase
+                        .from("valentinesmessages")
+                        .select(
+                            "id,sender,receiver,show_sender,message,background",
+                        );
+                    if (error) {
+                        enqueueSnackbar("Failed to load Valentines", {
+                            variant: "error",
+                        });
+                        return;
+                    }
+                    console.log(data);
+                }}
+            >
+                fetch all visible valentines
+            </AsyncButton>
+            <h2>fetched valentines ({valentines.length})</h2>
+            {valentines.map((valentine) => (
+                <p>{valentine}</p>
+            ))}
+        </>
+    );
 };
 
 export default Valentines;
