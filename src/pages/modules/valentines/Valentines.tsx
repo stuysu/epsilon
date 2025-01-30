4b204ff27997c852d369e7758ca71da24dcc4546
David Chen <dchen07@protonmail.com>
2025-01-29 21:40:29 -0500

    Add valentines 'editor' and proper printing of fetched data

--- src/pages/modules/valentines/Valentines.tsx
+++ src/pages/modules/valentines/Valentines.tsx
@@ -1,9 +1,12 @@
 import AsyncButton from "../../../comps/ui/AsyncButton";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { supabase } from "../../../supabaseClient";
 import { enqueueSnackbar } from "notistack";
+import { Input, InputBase } from "@mui/material";
+import UserContext from "../../../comps/context/UserContext";
 
-interface Valentine {
+type Valentine = {
+    [key: string]: any;
     id: number;
     sender: number;
     receiver: number;
@@ -11,20 +14,93 @@ interface Valentine {
 
     message: string;
     background: string;
-}
+};
 const Valentines = () => {
-    const [valentines, setValentines] = useState([]);
+    const user = useContext(UserContext);
+    const [valentines, setValentines] = useState<Valentine[]>([]);
+    const [receiver, setReceiver] = useState(0);
+    const [message, setMessage] = useState("");
+    const [background, setBackground] = useState("");
+    const [showSender, setShowSender] = useState(false);
 
     return (
         <>
             <h1>Valentines Testing</h1>
+            <p>
+                receiver
+                <InputBase
+                    placeholder="receiver"
+                    value={receiver}
+                    onChange={(e) => {
+                        let a = parseInt(e.target.value);
+                        if (isNaN(a) || a < 0) {
+                            a = 0;
+                        }
+                        setReceiver(a);
+                    }}
+                />
+            </p>
+            <p>
+                message
+                <InputBase
+                    value={message}
+                    onChange={(e) => {
+                        setMessage(e.target.value);
+                    }}
+                />
+            </p>
+            <p>
+                background
+                <InputBase
+                    value={background}
+                    onChange={(e) => {
+                        setBackground(e.target.value);
+                    }}
+                />
+            </p>
+            <p>
+                showSender
+                <Input
+                    type="checkbox"
+                    value={showSender}
+                    onChange={(_) => {
+                        setShowSender(!showSender);
+                    }}
+                />
+            </p>
+            <AsyncButton
+                onClick={async () => {
+                    const { data, error } = await supabase
+                        .from("valentinesmessages")
+                        .insert({
+                            sender: user.id,
+                            receiver,
+                            message,
+                            background,
+                            show_sender: showSender,
+                        });
+                    if (error) {
+                        enqueueSnackbar(
+                            `Failed to upload! Error: \`${error.message}\``,
+                            {
+                                variant: "error",
+                            },
+                        );
+                        return;
+                    }
+                    enqueueSnackbar("Uploaded!", { variant: "success" });
+                }}
+            >
+                upload valentine
+            </AsyncButton>
             <AsyncButton
                 onClick={async () => {
                     const { data, error } = await supabase
                         .from("valentinesmessages")
                         .select(
                             "id,sender,receiver,show_sender,message,background",
-                        );
+                        )
+                        .returns<Valentine[]>();
                     if (error) {
                         enqueueSnackbar("Failed to load Valentines", {
                             variant: "error",
@@ -32,14 +108,26 @@ const Valentines = () => {
                         return;
                     }
                     console.log(data);
+                    setValentines(data);
                 }}
             >
                 fetch all visible valentines
             </AsyncButton>
             <h2>fetched valentines ({valentines.length})</h2>
-            {valentines.map((valentine) => (
-                <p>{valentine}</p>
-            ))}
+            {valentines.map((valentine) =>
+                [
+                    "id",
+                    "sender",
+                    "receiver",
+                    "show_sender",
+                    "message",
+                    "background",
+                ].map((key: string) => (
+                    <p>
+                        {key}: {valentine[key]}
+                    </p>
+                )),
+            )}
         </>
     );
 };
