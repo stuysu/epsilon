ec18e2aa0c0f469bb3ded27d8c3c33d5fbeeac24
Restyled.io <commits@restyled.io>
2025-08-31 17:49:29 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,63 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content : [ "./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}" ],
-  theme : {
-    fontFamily : {
-      sans : [ "inter-variable", "system-ui", "sans-serif" ],
-      display :
-              [
+    content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        fontFamily: {
+            sans: ["inter-variable", "system-ui", "sans-serif"],
+            display: [
                 "neue-haas-grotesk-display",
                 "inter-variable",
                 "sans-serif",
-              ],
-      mono : [ "source-code-pro", "monospace" ],
-    },
-
-    extend : {
-      colors : {
-        bg : "var(--bg-main)",
-
-        layer : {
-          1 : "var(--layer-primary)",
-          2 : "var(--layer-secondary)",
-          3 : "var(--layer-tertiary)",
+            ],
+            mono: ["source-code-pro", "monospace"],
         },
 
-        typography : {
-          1 : "var(--text-primary)",
-          2 : "var(--text-secondary)",
-          3 : "var(--text-tertiary)",
-        },
+        extend: {
+            colors: {
+                bg: "var(--bg-main)",
 
-        accent : "var(--accent)",
-
-        blue : "var(--blue)",
-        green : "var(--green)",
-        yellow : "var(--yellow)",
-        red : "var(--red)",
-        beige : "var(--beige)",
-        divider : "var(--divider)",
-
-        blurDark : "var(--blur-dark)",
-        blurLight : "var(--blur-light)",
-      },
-      boxShadow : {
-        module : "0 0 1.5px 0 var(--shadow-module) inset, 0 16px 40px -8px rgba(0,0,0,0.12)",
-        control : "0 0 2px 0 var(--shadow-control) inset, 0 4px 12px -2px rgba(0,0,0,0.18)",
-      },
-    },
-  },
-  plugins :
-          [
-            function({addUtilities}) {
-              addUtilities({
-                ".scrollbar-none" : {
-                  "-ms-overflow-style" : "none",
-                  "scrollbar-width" : "none",
+                layer: {
+                    1: "var(--layer-primary)",
+                    2: "var(--layer-secondary)",
+                    3: "var(--layer-tertiary)",
                 },
-                ".scrollbar-none::-webkit-scrollbar" : {
-                  display : "none",
+
+                typography: {
+                    1: "var(--text-primary)",
+                    2: "var(--text-secondary)",
+                    3: "var(--text-tertiary)",
                 },
-              });
+
+                accent: "var(--accent)",
+
+                blue: "var(--blue)",
+                green: "var(--green)",
+                yellow: "var(--yellow)",
+                red: "var(--red)",
+                beige: "var(--beige)",
+                divider: "var(--divider)",
+
+                blurDark: "var(--blur-dark)",
+                blurLight: "var(--blur-light)",
             },
-          ],
+            boxShadow: {
+                module: "0 0 1.5px 0 var(--shadow-module) inset, 0 16px 40px -8px rgba(0,0,0,0.12)",
+                control:
+                    "0 0 2px 0 var(--shadow-control) inset, 0 4px 12px -2px rgba(0,0,0,0.18)",
+            },
+        },
+    },
+    plugins: [
+        function ({ addUtilities }) {
+            addUtilities({
+                ".scrollbar-none": {
+                    "-ms-overflow-style": "none",
+                    "scrollbar-width": "none",
+                },
+                ".scrollbar-none::-webkit-scrollbar": {
+                    display: "none",
+                },
+            });
+        },
+    ],
 };
