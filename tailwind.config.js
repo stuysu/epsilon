829abb27eaa7faebc614e3fcb4c94ffdad2419cb
Restyled.io <commits@restyled.io>
2025-09-19 05:00:29 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,64 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        fontFamily: {
-            sans: ["inter-variable", "system-ui", "sans-serif"],
-            display: [
+  content : [ "./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}" ],
+  theme : {
+    fontFamily : {
+      sans : [ "inter-variable", "system-ui", "sans-serif" ],
+      display :
+              [
                 "neue-haas-grotesk-display",
                 "inter-variable",
                 "sans-serif",
-            ],
-            mono: ["source-code-pro", "monospace"],
-        },
-
-        extend: {
-            colors: {
-                bg: "var(--bg-main)",
-
-                layer: {
-                    1: "var(--layer-primary)",
-                    2: "var(--layer-secondary)",
-                    3: "var(--layer-tertiary)",
-                },
-
-                typography: {
-                    1: "var(--text-primary)",
-                    2: "var(--text-secondary)",
-                    3: "var(--text-tertiary)",
-                },
-
-                accent: "var(--accent)",
-
-                blue: "var(--blue)",
-                green: "var(--green)",
-                yellow: "var(--yellow)",
-                red: "var(--red)",
-                beige: "var(--beige)",
-                divider: "var(--divider)",
-
-                blurDark: "var(--blur-dark)",
-                blurLight: "var(--blur-light)",
-            },
-            boxShadow: {
-                module:
-                    "0 4px 20px 0 var(--shadow-base), 0 4px 3px 0 var(--shadow-base), 0 0 3px 0 var(--shadow-antithesis) inset, 0 -7px 20px 0 var(--shadow-fume) inset",
-                control:
-                    "0 4px 4px 0 var(--shadow-base), 0 0 1px 0 var(--shadow-antithesis) inset, 0 1px 3px 0 var(--shadow-decoration) inset",
-            },
-        },
+              ],
+      mono : [ "source-code-pro", "monospace" ],
     },
-    plugins: [
-        function ({ addUtilities }) {
-            addUtilities({
-                ".scrollbar-none": {
-                    "-ms-overflow-style": "none",
-                    "scrollbar-width": "none",
-                },
-                ".scrollbar-none::-webkit-scrollbar": {
-                    display: "none",
-                },
-            });
+
+    extend : {
+      colors : {
+        bg : "var(--bg-main)",
+
+        layer : {
+          1 : "var(--layer-primary)",
+          2 : "var(--layer-secondary)",
+          3 : "var(--layer-tertiary)",
         },
-    ],
+
+        typography : {
+          1 : "var(--text-primary)",
+          2 : "var(--text-secondary)",
+          3 : "var(--text-tertiary)",
+        },
+
+        accent : "var(--accent)",
+
+        blue : "var(--blue)",
+        green : "var(--green)",
+        yellow : "var(--yellow)",
+        red : "var(--red)",
+        beige : "var(--beige)",
+        divider : "var(--divider)",
+
+        blurDark : "var(--blur-dark)",
+        blurLight : "var(--blur-light)",
+      },
+      boxShadow : {
+        module : "0 4px 20px 0 var(--shadow-base), 0 4px 3px 0 var(--shadow-base), 0 0 3px 0 var(--shadow-antithesis) inset, 0 -7px 20px 0 var(--shadow-fume) inset",
+        control : "0 4px 4px 0 var(--shadow-base), 0 0 1px 0 var(--shadow-antithesis) inset, 0 1px 3px 0 var(--shadow-decoration) inset",
+      },
+    },
+  },
+  plugins :
+          [
+            function({addUtilities}) {
+              addUtilities({
+                ".scrollbar-none" : {
+                  "-ms-overflow-style" : "none",
+                  "scrollbar-width" : "none",
+                },
+                ".scrollbar-none::-webkit-scrollbar" : {
+                  display : "none",
+                },
+              });
+            },
+          ],
 };
