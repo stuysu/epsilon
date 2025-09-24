f7a7436ccb504823513b6da11f69c471033c5c6c
Rahuldeb5 <rahuldeb.usa@gmail.com>
2025-09-24 15:30:12 -0400

    Made changes to display complete list of rooms available and show current reserved meetings

--- src/modules/stuyactivities/orgs/org_admin/components/MeetingAttendanceCard.tsx
+++ src/modules/stuyactivities/orgs/org_admin/components/MeetingAttendanceCard.tsx
@@ -5,20 +5,28 @@ const MeetingAttendanceCard = ({
     id,
     room,
     startTime,
+    clickable,
 }: {
     title: string;
     id: number;
     room?: string;
     startTime: string;
+    clickable: boolean;
 }) => {
     const navigate = useNavigate();
+
+    const handleClick = () => {
+        if (clickable) {
+            navigate(`${id}`);
+        }
+    };
 
     return (
         <div
             className={
                 "transition-colors bg-layer-1 w-full mb-6 p-6 rounded-xl shadow-control cursor-pointer sm:hover:bg-layer-2"
             }
-            onClick={() => navigate(`${id}`)}
+            onClick={() => handleClick()}
         >
             <h3>{title}</h3>
             <p>{new Date(startTime).toLocaleString()}</p>
