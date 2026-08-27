72f3d7fa6c521b6d90de57ba3288881db9d02dd4
willzhang <hello@willzhang.info>
2026-08-27 14:39:52 -0500

    Remove UserProvider redundant wrap

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import UserProvider from "./contexts/UserProvider";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
 );
-root.render(
-    <UserProvider>
-        <App />
-    </UserProvider>,
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
