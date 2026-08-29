82eb14b57ea222ad67c3765beb4776fff9ac8acb
webcubed <77589042+webcubed@users.noreply.github.com>
2026-08-28 21:54:29 -0400

    newestier anon key trust last time

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,4 +4,4 @@ export const SUPABASE_URL =
     process.env.REACT_APP_SUPABASE_URL || "https://base.stuysu.org/";
 export const SUPABASE_ANON_KEY =
     process.env.REACT_APP_SUPABASE_ANON_KEY ||
-    "sb_publishable_5TA2reuePESqqHzMCWpQnz_fITQWJED";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg3OTY4Mzk2LCJleHAiOjE5NDU2NDgzOTZ9.Ajo4QUjgnsVUYKjTW7rdDYOUpLI3X-A7SWQFVtFrhmg";
