fbd588e0a8e702159ff46c599f53570eaed6bf37
willzhang <hello@willzhang.info>
2026-08-29 01:03:28 -0500

    Customizability for TextInput

--- src/components/ui/input/TextInput.tsx
+++ src/components/ui/input/TextInput.tsx
@@ -1,6 +1,9 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, InputHTMLAttributes } from "react";
 
-type TextInputProps = {
+type TextInputProps = Omit<
+    InputHTMLAttributes<HTMLInputElement>,
+    "value" | "onChange"
+> & {
     placeholder?: string;
     value: string;
     onChange: (value: string) => void;
@@ -10,23 +13,27 @@ export default function TextInput({
     placeholder = "Send a message...",
     value,
     onChange,
+    className = "",
+    type = "text",
+    ...props
 }: TextInputProps) {
     return (
         <input
-            type="text"
+            {...props}
+            type={type}
             placeholder={placeholder}
             value={value}
             onChange={(e: ChangeEvent<HTMLInputElement>) =>
                 onChange(e.target.value)
             }
-            className=" w-full rounded-xl px-4 h-11 transition-colors
+            className={`w-full rounded-xl px-4 h-11 transition-colors
                 text-typography-1
                 important
               bg-layer-2
               sm:hover:bg-layer-3
               focus:outline-1
               shadow-inner
-        "
+              ${className}`}
         />
     );
 }
