850703476a3d7390e1caf372f2c79bb3ae7eea84
webcubed <77589042+webcubed@users.noreply.github.com>
2026-08-28 21:45:52 -0400

    even newer anon key

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,4 +4,4 @@ export const SUPABASE_URL =
     process.env.REACT_APP_SUPABASE_URL || "https://base.stuysu.org/";
 export const SUPABASE_ANON_KEY =
     process.env.REACT_APP_SUPABASE_ANON_KEY ||
-    "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImUwYWE3YTg3MzU3ZGUyMmQifQ.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg3OTY1NjYxLCJleHAiOjIxMDMzMjU2NjF9.pZlWKM_QeNmAP0QF35PrFOB9Ym-VlEqWbrOlLhVk73dSOLkcRAW-BJU5uU2wOkGBIEhjJFYMQTzI7rFu5scxCg";
+    "sb_publishable_5TA2reuePESqqHzMCWpQnz_fITQWJED";
